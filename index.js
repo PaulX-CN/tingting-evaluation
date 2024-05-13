4f88a6ef01880d920fc2093add5855becbb61a30
Tingting Chen <ttina.chenn@gmail.com>
2024-05-13 18:34:36 -0400

    added pagination

--- index.js
+++ index.js
@@ -67,9 +67,11 @@ const Model = (() => {
     #onChange;
     #inventory;
     #cart;
+    #page;
     constructor() {
       this.#inventory = [];
       this.#cart = [];
+      this.#page = sessionStorage.getItem("page")??1;
     }
     get cart() {
       return this.#cart;
@@ -78,6 +80,13 @@ const Model = (() => {
     get inventory() {
       return this.#inventory;
     }
+    get pages() {
+      return Math.ceil(Number(this.#inventory.length)/2);
+    }
+
+    get page() {
+      return this.#page;
+    }
 
     set cart(newCart) {
       this.#cart = newCart;
@@ -85,6 +94,10 @@ const Model = (() => {
     }
     set inventory(newInventory) {
       this.#inventory = newInventory;
+      this.#onChange();
+    }
+    set page(pageNumber) {
+      this.#page = pageNumber;
       this.#onChange();
     }
 
@@ -116,9 +129,11 @@ const View = (() => {
   const inventoryEl = document.querySelector(".inventory-item");
   const cartEl = document.querySelector(".cart-item");
   const checkoutEl = document.querySelector(".checkout-btn");
-  const renderInventory = (inventory) => {
+  const pageEl = document.querySelector(".pages");
+  const renderInventory = (inventory, pageId) => {
     let inventoryList = "";
-    inventory.forEach((item)=>{
+    const newInventory = inventory.slice((pageId-1)*2, pageId*2);
+    newInventory.forEach((item)=>{
       let count = item.count??0;
       const listItem = `<li id="inventory-${item.id}"><span>${item.content}</span><button class="updateAmount minus">-</button>${count}<button class="updateAmount plus">+</button><button class="cart">add to cart</button></li>`;
       inventoryList+=listItem;
@@ -135,8 +150,18 @@ const View = (() => {
     })
     cartEl.innerHTML = cartList;
   }
+  
+  const renderPages = (pages, page) => {
+    let pageNumberItems = "";
+    for(let i=1; i <= pages; i++){
+      const pageNumberItem = `<span id="page-${i}" class="page-number ${page == i?"active":""}">${i}</span>`;
+      pageNumberItems+=pageNumberItem;
+    }
+    pageEl.innerHTML = pageNumberItems;
 
-  return {renderInventory, renderCarts, inventoryEl, cartEl, checkoutEl};
+  }
+
+  return {renderInventory, renderCarts, renderPages, inventoryEl, cartEl, checkoutEl, pageEl};
 })();
 
 const Controller = ((view, model) => {
@@ -153,7 +178,6 @@ const Controller = ((view, model) => {
   };
   const handleUpdateAmount = () => {
     view.inventoryEl.addEventListener("click", (event) => {
-      // event.preventDefault();
       const element = event.target;
         const content = element.innerText;
         const itemId = Number(element.parentElement.getAttribute("id").split("-")[1]);
@@ -173,7 +197,6 @@ const Controller = ((view, model) => {
 
   const handleAddToCart = () => {
     view.inventoryEl.addEventListener("click", (event) => {
-      // event.preventDefault();
       const element = event.target;
       const itemId = Number(element.parentElement.getAttribute("id").split("-")[1]);
       if(element.className == "cart") {
@@ -187,7 +210,6 @@ const Controller = ((view, model) => {
             ...state.inventory[itemId-1],
             count: count,
           }).then(() => {
-            // state.cart = [data];
             return model.getCart();
           }).then((data) => state.cart = data);
         } else {
@@ -195,7 +217,6 @@ const Controller = ((view, model) => {
             itemId,
             previousItem.count + count,
           ).then((data) => {
-            // state.cart = state.cart.map((item) => (item.id !== item.id));
             model.getCart().then((data) => {
               state.cart = data;
             });
@@ -206,6 +227,16 @@ const Controller = ((view, model) => {
       }
     })
   };
+  
+  const handlePageClick = () => {
+    view.pageEl.addEventListener("click", (event) => {
+      const element = event.target;
+      const itemId = Number(element.getAttribute("id").split("-")[1]);
+      state.page = itemId;
+      sessionStorage.setItem("page", itemId);
+
+    })
+  }
 
   const handleDelete = () => {
     view.cartEl.addEventListener("click", (event)=>{
@@ -231,13 +262,15 @@ const Controller = ((view, model) => {
   const bootstrap = () => {
     init();
     state.subscribe(()=>{
-      view.renderInventory(state.inventory);
+      view.renderInventory(state.inventory, state.page);
       view.renderCarts(state.cart);
+      view.renderPages(state.pages, state.page);
     })
     handleUpdateAmount();
     handleAddToCart();
     handleDelete();
     handleCheckout();
+    handlePageClick();
   };
   return {
     bootstrap,
